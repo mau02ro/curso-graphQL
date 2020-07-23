a83b2681cfe7e96a5a68fa871d1359f780098018
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-07-23 03:35:52 -0430

    graphql ok

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 "use strict";
 
-require('dotenv').config()
+require("dotenv").config();
 const { makeExecutableSchema } = require("graphql-tools");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
+const cors = require("cors");
 
 const app = express();
 app.set("PORT", process.env.port || 3000);
+debugger;
 
 // definiendo el esquema
 const typeDefs = readFileSync(
   join(__dirname, "lib", "schema.graphql")
 ).toString();
 const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(cors());
 
 // Definimos una ruta para '/api' que va a ejecutar el graphqlHTTP
 app.use(
